a5653f500009c7624867ee64d29b9d240e436b08
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-10-29 00:16:06 +0200

    feat: added axios and config

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Container, Box } from "@mui/material";
 // import { useTranslations } from "next-intl";
 import styles from "./page.module.css";
+import HealthCheck from "./components/HealthCheck";
 
 export default function HomePage() {
   // const t = useTranslations("HomePage");
@@ -8,6 +9,7 @@ export default function HomePage() {
   return (
     <Container component="main">
       <Box className={styles.mainWrapper}></Box>
+      <HealthCheck />
     </Container>
   );
 }
