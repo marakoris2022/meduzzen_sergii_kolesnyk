918420eb7b5bd2c22816ccb6ebd25f3aaffdccf9
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-10-29 16:14:31 +0200

    feat: added auth pages

---
+++ src/app/(views)/signin/page.tsx
@@ -0,0 +1,11 @@
+import { Box, Container } from "@mui/material";
+
+const SignInPage = () => {
+  return (
+    <Container>
+      <Box>FORM WILL BE HERE</Box>
+    </Container>
+  );
+};
+
+export default SignInPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,20 @@
-import { Container, Box } from "@mui/material";
+import { Container, Box, Typography, Button } from "@mui/material";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function HomePage() {
   return (
     <Container component="main">
-      <Box className={styles.mainWrapper}></Box>
+      <Box className={styles.mainWrapper}>
+        <Typography>Welcome to the Quiz Application!</Typography>
+        <Typography>Have you an account?</Typography>
+        <Link href={"/signin"}>
+          <Button>Sign In</Button>
+        </Link>
+        <Link href={"/signup"}>
+          <Button>Sign Up</Button>
+        </Link>
+      </Box>
     </Container>
   );
 }
