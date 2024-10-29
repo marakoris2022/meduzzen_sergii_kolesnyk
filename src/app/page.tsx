00160c6748c5daeb91f42a4933f82091bfb12210
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-10-29 19:48:51 +0200

    feat: added translation to the main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,25 @@
 import { Container, Box, Typography, Button } from "@mui/material";
 import styles from "./page.module.css";
 import Link from "next/link";
+import { useTranslations } from "next-intl";
 
 export default function HomePage() {
+  const t = useTranslations("HomePage");
+
   return (
     <Container component="main">
       <Box className={styles.mainWrapper}>
-        <Typography>Welcome to the Quiz Application!</Typography>
-        <Typography>Have you an account?</Typography>
-        <Link href={"/signin"}>
-          <Button>Sign In</Button>
+        <Typography className={styles.title}>{t("title")}</Typography>
+        <Typography>{t("account")}</Typography>
+        <Link className={styles.link} href={"/signin"}>
+          <Button variant="outlined" fullWidth>
+            {t("signin")}
+          </Button>
         </Link>
-        <Link href={"/signup"}>
-          <Button>Sign Up</Button>
+        <Link className={styles.link} href={"/signup"}>
+          <Button variant="outlined" fullWidth>
+            {t("signup")}
+          </Button>
         </Link>
       </Box>
     </Container>
