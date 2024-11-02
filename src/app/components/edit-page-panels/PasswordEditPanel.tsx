9b69ba60fabb4014a75a62c1d85b24397864c590
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-11-03 00:57:42 +0200

    feat: password change added

--- src/app/components/edit-page-panels/PasswordEditPanel.tsx
+++ src/app/components/edit-page-panels/PasswordEditPanel.tsx
@@ -1,7 +1,116 @@
-import { Box } from "@mui/material";
+import { passwordValidation } from "@/constants/validationSchemas";
+import { Button, Stack, Switch, TextField, Typography } from "@mui/material";
+import { useTranslations } from "next-intl";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import VisibilityOutlinedIcon from "@mui/icons-material/VisibilityOutlined";
+import VisibilityOffOutlinedIcon from "@mui/icons-material/VisibilityOffOutlined";
+import { updateUserPassword } from "@/services/axios-api-methods/axiosPut";
+import { useUserData } from "@/app/hooks/useUserData";
+
+type EditPasswordFromProps = {
+  password: string;
+  confirmPassword: string;
+};
+
+type UpdateStatusType = {
+  text: string;
+  color: "success" | "error";
+};
+
+const updateStatusInit: UpdateStatusType = {
+  text: "",
+  color: "success",
+};
 
 const Password = () => {
-  return <Box>Password</Box>;
+  const t = useTranslations("EditProfilePassword");
+  const { userData } = useUserData();
+  const [isHidden, setIsHidden] = useState<boolean>(true);
+  const [updateStatus, setUpdateStatus] =
+    useState<UpdateStatusType>(updateStatusInit);
+
+  const {
+    register,
+    handleSubmit,
+    clearErrors,
+    reset,
+    getValues,
+    formState: { errors },
+  } = useForm<EditPasswordFromProps>({
+    defaultValues: {},
+  });
+
+  function handleReset() {
+    clearErrors();
+    reset();
+    setUpdateStatus((state) => ({ ...state, text: "" }));
+  }
+
+  async function submit(data: EditPasswordFromProps) {
+    try {
+      await updateUserPassword({
+        user_password: data.password,
+        user_password_repeat: data.confirmPassword,
+        user_id: userData!.user_id,
+      });
+      setUpdateStatus({ text: t("updated"), color: "success" });
+    } catch {
+      setUpdateStatus({ text: t("wrong"), color: "error" });
+    }
+  }
+
+  function handleSwitch() {
+    setIsHidden((state) => !state);
+  }
+
+  return (
+    <Stack onSubmit={handleSubmit(submit)} component={"form"} gap={3}>
+      <Stack direction={"row"} alignItems={"center"}>
+        <VisibilityOffOutlinedIcon color={!isHidden ? "disabled" : "primary"} />
+        <Switch onChange={handleSwitch} />
+        <VisibilityOutlinedIcon color={isHidden ? "disabled" : "primary"} />
+      </Stack>
+
+      <TextField
+        autoComplete="false"
+        type={isHidden ? "password" : "text"}
+        {...register("password", passwordValidation(t))}
+        label={t("nPassword")}
+        error={!!errors.password}
+        helperText={errors.password?.message || ""}
+      ></TextField>
+      <TextField
+        autoComplete="false"
+        type={isHidden ? "password" : "text"}
+        {...register("confirmPassword", {
+          required: t("Confirm Required"),
+          validate: (value) =>
+            value === getValues("password") || t("Passwords Match"),
+        })}
+        label={t("cPassword")}
+        error={!!errors.confirmPassword}
+        helperText={errors.confirmPassword?.message || ""}
+      ></TextField>
+
+      <Typography textAlign={"center"} color={updateStatus.color}>
+        {updateStatus.text}
+      </Typography>
+      <Stack
+        justifyContent={"center"}
+        alignItems={"center"}
+        direction={"row"}
+        gap={3}
+      >
+        <Button type="submit" variant="outlined" color="success">
+          {t("Submit")}
+        </Button>
+        <Button onClick={handleReset} variant="outlined" color="warning">
+          {t("Clear")}
+        </Button>
+      </Stack>
+    </Stack>
+  );
 };
 
 export default Password;
--- src/services/axios-api-methods/axiosPut.ts
+++ src/services/axios-api-methods/axiosPut.ts
@@ -9,11 +9,25 @@ export type UpdateUserGeneralProps = {
   user_links: string[];
 };
 
+export type UpdateUserPasswordProps = {
+  user_password: string;
+  user_password_repeat: string;
+  user_id: number;
+};
+
 export const updateUserGeneralData = async (
   data: UpdateUserGeneralProps,
   userId: number
 ) => {
   return await axiosInstance.put(`/user/${userId}/update_info/`, {
     ...data,
+  });
+};
+
+export const updateUserPassword = async (data: UpdateUserPasswordProps) => {
+  const { user_password, user_password_repeat, user_id } = data;
+  return await axiosInstance.put(`/user/${user_id}/update_password/`, {
+    user_password,
+    user_password_repeat,
   });
 };
