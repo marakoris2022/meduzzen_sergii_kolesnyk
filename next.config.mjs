4ed696540a867015760fea57144414dce6aa747e
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-11-05 12:42:01 +0200

    feat: added next.config data to env

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,19 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
+const IMG_PROTOCOL = process.env.IMG_PROTOCOL || "http";
+const IMG_HOSTNAME = process.env.IMG_HOSTNAME || "51.20.210.187";
+
+console.log("next.config", IMG_PROTOCOL, IMG_HOSTNAME);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "51.20.210.187",
+        protocol: IMG_PROTOCOL,
+        hostname: IMG_HOSTNAME,
       },
     ],
   },
