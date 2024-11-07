95512ce6914ae527ea3cc5c91983e4d719854061
SERGIY KOLESNYK <114981277+marakoris2022@users.noreply.github.com>
2024-11-07 15:57:59 +0200

    feat: updated due to the problem with Vercel env

--- next.config.mjs
+++ next.config.mjs
@@ -2,17 +2,14 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin();
 
-const IMG_SERVER_PROTOCOL = process.env.IMG_SERVER_PROTOCOL;
-const IMG_SERVER_HOSTNAME = process.env.IMG_SERVER_HOSTNAME;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "standalone",
   images: {
     remotePatterns: [
       {
-        protocol: IMG_SERVER_PROTOCOL,
-        hostname: IMG_SERVER_HOSTNAME,
+        protocol: process.env.NEXT_PUBLIC_IMG_SERVER_PROTOCOL,
+        hostname: process.env.NEXT_PUBLIC_IMG_SERVER_HOSTNAME,
       },
     ],
   },
